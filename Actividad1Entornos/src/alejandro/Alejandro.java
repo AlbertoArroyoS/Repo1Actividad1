1a78e20c3d574301781a7b9db6b3f4aaad8a9575
Ale Aguiar Díaz <aleaguiard@hotmail.com>
2023-02-04 12:19:30 +0000

    Añadido hobbies

--- Actividad1Entornos/src/alejandro/Alejandro.java
+++ Actividad1Entornos/src/alejandro/Alejandro.java
@@ -5,7 +5,7 @@ public class Alejandro {
 	public static void main(String[] args) {
 		imprimirNombre();
 		System.out.println("Requerimiento 2 - Alejandro");
-		
+		System.out.println("3 hobbies: Hacer deporte, leer y escuchar música");
 	}
 
 	public static void imprimirNombre () {

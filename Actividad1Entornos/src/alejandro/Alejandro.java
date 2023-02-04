283b594dd85620cabf9aea3636aa74647294d975
Ale Aguiar Díaz <aleaguiard@hotmail.com>
2023-02-04 11:42:47 +0000

    Creados paquete y clase Alejandro. Método imprimir nombre

---
+++ Actividad1Entornos/src/alejandro/Alejandro.java
@@ -0,0 +1,14 @@
+package alejandro;
+
+public class Alejandro {
+
+	public static void main(String[] args) {
+		imprimirNombre();
+
+	}
+
+	public static void imprimirNombre () {
+		System.out.println("Hola, soy Alejandro");
+	}
+	
+}
